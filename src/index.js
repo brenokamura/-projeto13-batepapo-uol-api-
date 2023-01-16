8b345deeff1fa811c8247f31ecbea58e75201def
brenokamura <96897837+brenokamura@users.noreply.github.com>
2023-01-16 14:46:49 -0300

    Post Participants implementado

--- src/index.js
+++ src/index.js
@@ -8,9 +8,21 @@ import dayjs from 'dayjs';
 dotenv.config();
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 const participantSchema = joi.object({
 	name: joi.string().min(3).required(),
 	lastStatus: joi.number()
+}
+);
+
+const messageSchema = joi.object({
+	from: joi.string().required(),
+	to: joi.string().min(3).required(),
+	text: joi.string().min(1).required(),
+	type: joi.string().valid("message", "private_message").required(),
+	time: joi.string()
 }
 );
 
@@ -23,9 +35,37 @@ try{
     console.log(err);
 }
 
-const db = mongoClient.db("batepapouol");
+const db = mongoClient.db();
 
 app.post("/participants", async (req, res) => {
+
+	const name = req.body;
+	const validation = participantSchema.validate(name, { abortEarly: false, });
+
+	if (validation.error) {
+		const errors = validation.error.details.map((type) => type.message);
+		res.status(422).send(errors);
+		return;
+	}
+	try {
+		const isParticipant = await db.collection("participants").findOne({ name })
+		if (isParticipant) {
+			res.sendStatus(409);
+			return;
+		}
+		await db.collection("participants").insertOne({ name, lastStatus: Date.now() });
+		await db.collection("message").insertOne({
+			from: name,
+			to: "Todos",
+			text: "entrar na sala...",
+			type: "status",
+			time: dayjs().format("HH:mm:ss"),	
+		});
+		res.send(201);
+	} catch (error) {
+		res.status(500).send(error.message);
+	}
+})
 });                 
 
 app.listen(5000, () => console.log("Rodando a porta 5000. Sucesso!!!"))
